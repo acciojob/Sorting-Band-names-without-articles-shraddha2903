4251d886f42c666f076a6185dc9803c9f16fbfe5
shraddha2903 <70905602+shraddha2903@users.noreply.github.com>
2023-08-14 17:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 //your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let noArticle={};
+
+let temp=[];
+for(int i=0;i<touristSpots.length;i++)
+{
+	let t=touristSpots[i].split("");//The ,Virupaksha ,Temple
+	let nArt="";
+	for(let j=0;j<t.length;j++)
+		{
+			let c=t[j].toLowerCase();
+			if(c!='a' || c!='an' || c!='the')
+			{
+				nArt=nArt+t[j]+" ";
+				console.log(nArt);
+			}
+		}
+	nArt.trim();
+	temp.push(nArt);
+	setattr(noArticle, nArt, touristSpots[i]);
+	
+}
+
+document.getElementById("bands").innerHTML=temp;
+
+
+
+
 
